918932cad4e5f658baed1eadb4811e632af9ff97
samuelmambo <museemukuu@gmail.com>
2022-06-28 13:58:14 +0300

    add

--- about-us.php
+++ about-us.php
@@ -61,47 +61,7 @@ if( isset($_POST['submitButton']))
          </div>
      </nav> -->
      <!-- Navbar design ends here -->
-     <main class="p-5 bg-light mb-4">
-        <h1>About us</h1>
-        <p>
-            This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.
-        </p>
-     </main>
-     <div class="container">
-        <div class="row">
-            <div class="col-lg-4">
-                <h1>Our program</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam commodi dolore vero praesentium nisi quaerat culpa ea unde, quos distinctio incidunt, velit ad! Beatae vel a voluptate architecto quaerat saepe commodi, quis in nobis, aliquid corrupti repellendus ab, vero ipsa?
-                </p>
-            </div>
-            <div class="col-lg-6">
-                <img src="images/rose.jpg" alt="rose image" class="rounded">
-            </div>
-        </div>
-        <div class="row">
-            <div class="card" style="width: 18rem;">
-                <h1>Skill discovery</h1>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic delectus officiis nobis accusamus magnam?
-                </p>
-                <button class="btn btn-primary">View details</button>
-            </div>
-            <div class="card" style="width: 18rem; ">
-                <h1>Upskill program</h1>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic delectus officiis nobis accusamus magnam?
-                </p>
-                <button class="btn btn-primary">View details</button>
-            </div>
-            <div class="card" style="width: 18rem;">
-                <h1>Path finding Program</h1>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic delectus officiis nobis accusamus magnam?
-                </p>
-                <button class="btn btn-primary">View details</button>
-            </div>                      
-        </div>
+     
         <div class="row">
             <p>Subscribe to get information, latest news about Zalego Academy</p>
             <form action="about-us.php" method="POST">
--- enroll.php
+++ enroll.php
@@ -44,7 +44,7 @@ if( isset($_POST['submitButton']))
 <body>
        <!--navbar starts here-->
     <nav class="navbar navbar-expand-lg bg-light fixed-top-shadow">
-        <div class="container-fluid">
+        <div class="container">
             <a href="#" class="navbar-brand">Zalego Academy</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#menus">
                 <span class="navbar-toggler-icon"></span>
@@ -53,21 +53,23 @@ if( isset($_POST['submitButton']))
                 <div class="navbar-nav">
                     <a href="home.html" class="nav-link active">Home</a>
                     <a href="#" class="nav-link">About Us</a>
-                    <a href="#" class="nav-link btn btn-primary"> Register now</a>
+                    <a href="#" class="nav-link btn btn-primary text-white"> Register now</a>
                 </div>
             </div>
         </div>
     </nav>
-    <main class="p-5 bg-light  text-primary  mb-4 ">
-        <h1 >JULY SOFTWARE ENGINEERING BOOTCAMP</h1>
+    <div class="container">
+     <main class="p-5 bg-secondary text-primary  mb-4 ">
+       <h1 >JULY SOFTWARE ENGINEERING BOOTCAMP</h1>
         <span class="text-dark"> <i class="fa fa-calendar-check-o fa-3x"></i></span>
         <span class="text-dark">20th July 2020</span> <br>
         <span class="text-dark"> <i class="fa fa-map-marker fa-3x"></i>
             <span class="text-dark">Zalego Academy <br>
                                       Devan Plaza
         </span></span>
-        
-    </main>
+       </div>
+     </main>
+    </div>
     <div>
         <p class="text-center">Looking for a place to kick start your career in Technology? <br>
         Whether you're a local, new in town or just crushing through we've got  <br>
@@ -81,21 +83,21 @@ if( isset($_POST['submitButton']))
             <div class="row"> 
                <div class="mb-3 col-lg-6 ">
                     <label for="firstname" class="form-label">Full Name</label>
-                    <input type="text"  name="fullmame" class="form-control" placeholder="Enter your full name" >
+                    <input type="text"  name="fullname" class="form-control shadow p-3 mb-5 bg-white rounded" placeholder="Enter your full name" >
                 </div>
                 <div class="mb-3 col-lg-6">
                     <label for="Phone Number" class="form-label">Phone Number</label>
-                    <input type="text" name="phonenumber" class="form-control" placeholder="+2547" >
+                    <input type="text" name="phonenumber" class="form-control shadow p-3 mb-5 bg-white rounded" placeholder="+2547" >
                 </div>    
             </div>
             <div class="row"> 
                 <div class="mb-3 col-lg-6 ">
                     <label for="Email Address" class="form-label">Email Address</label>
-                    <input type="tel"  name="emailaddress"class="form-control" placeholder="Please enter your email" >
+                    <input type="tel"  name="emailaddress"class="form-control shadow p-3 mb-5 bg-white rounded" placeholder="Please enter your email" >
                 </div>
                 <div class="mb-3 col-lg-6">
-                    <label for="Gender"  class="form-label">What's your gender?</label>
-                    <select  name="gender"class="form-select" >
+                    <label for="Gender"  class="form-label ">What's your gender?</label>
+                    <select  name="gender"class="form-select shadow p-3 mb-5 bg-white rounded" >
                         <option >--Select your gender--</option>
                         <option value="male">male</option>
                         <option value="female">female</option>
@@ -114,10 +116,12 @@ if( isset($_POST['submitButton']))
             </div>
             <div>
                 <label for="course" class="form-labl=el">What's your preffered course?</label>
-                <select class="form-select" name="course" >
+                <select class="form-select shadow p-3 mb-5 bg-white rounded" name="course">
                     <option >--Select your course--</option>
-                    <option value="Java">Java</option>
-                    <option value="Pyhton">Pyhton</option>
+                    <option value="Android App Development">Android App Development</option> 
+                    <option value="Web Design & Development">Web Design & Development</option>
+                    <option value="Data Analysis">Data Analysis</option>
+                    <option value="Cyber Security">Cyber Security</option>
                 </select>
             </div>
             <p>
@@ -143,7 +147,7 @@ if( isset($_POST['submitButton']))
         </p>
         <label for="email"></label>
         <input type="text" placeholder="Your email address">
-        <button type="submit" name="submitButton" class="btn btn-primary">Subscribe</button>
+        <button type="submit" name="submitButton" class="btn btn-primary" >Subscribe</button>
     </div>
     <hr>
     <footer>
